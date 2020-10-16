8f1bc37f78a1f5e8940197871bc63e8a0c1ff76c
tj.zhao <csdk@outlook.com>
2020-10-16 11:54:59 +0800

    starter with koa router

--- examples/starter/serverless.js
+++ examples/starter/serverless.js
@@ -1,11 +1,25 @@
 const { createServer, proxy } = require('../../index')
 const Koa = require('koa')
+const cors = require('@koa/cors')
+const bodyParser = require('koa-bodyparser')
+const Router = require('@koa/router')
 
 const app = new Koa()
+const router = new Router()
 
-app.use(async ctx => {
-  ctx.body = 'Hello, world!';
+app.use(cors())
+app.use(bodyParser())
+
+router.get('/', (ctx) => {
+  ctx.body = 'Hello, World!'
 })
+
+router.post('/post', (ctx) => {
+  ctx.body = ctx.request.body
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 const server = createServer(app.callback())
 module.exports.handler = (event, context, callback) => proxy(server, event, context, callback)
